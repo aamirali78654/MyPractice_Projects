37171561603352aee7a5ab83f3b9aa3558f06b7a
aamirali78654 <aamiralisargani786@gmail.com>
2024-08-10 20:15:15 +0500

    demo

--- thirdProject/app/src/main/java/com/example/demo/MainActivity.java
+++ thirdProject/app/src/main/java/com/example/demo/MainActivity.java
@@ -1,5 +1,6 @@
 package com.example.demo;
 
+import android.content.Intent;
 import android.os.Bundle;
 
 import androidx.activity.EdgeToEdge;
@@ -15,10 +16,10 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
         setContentView(R.layout.activity_main);
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
-            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
-            return insets;
-        });
+
+        Intent intent = new Intent(this, null);
+
+
+
     }
 }
