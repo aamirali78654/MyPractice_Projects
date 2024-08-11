5d12ed2b99cd2e07f5b2d83d78889bc6d5e530ac
aamirali78654 <aamiralisargani786@gmail.com>
2024-08-11 15:20:16 +0500

    AlertDialog Box Demo Practice_Based

---
+++ forthPractice/app/src/main/java/com/example/alertdialog_practice/One/MainActivity.java
@@ -0,0 +1,57 @@
+package com.example.alertdialog_practice.One;
+
+import android.app.Dialog;
+import android.content.DialogInterface;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.Toast;
+
+import androidx.activity.EdgeToEdge;
+import androidx.appcompat.app.AlertDialog;
+import androidx.appcompat.app.AppCompatActivity;
+
+import com.example.alertdialog_practice.R;
+
+public class MainActivity extends AppCompatActivity {
+    Button button;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        EdgeToEdge.enable(this);
+        setContentView(R.layout.activity_main);
+
+        button = findViewById(R.id.button);
+
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                AlertDialog.Builder ad = new AlertDialog.Builder(MainActivity.this);
+                ad.setTitle("Clear Window!!");
+                ad.setMessage("There is no more longer then this Window??");
+                ad.setPositiveButton("Yes", new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialogInterface, int i) {
+                        Toast.makeText(MainActivity.this, "you clicked Yes.", Toast.LENGTH_SHORT).show();
+                    }
+                });
+                ad.setNegativeButton("No", new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialogInterface, int i) {
+                        Toast.makeText(MainActivity.this, "you clicked No.", Toast.LENGTH_SHORT).show();
+                    }
+                });
+                ad.setNeutralButton("Cancel", new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialogInterface, int i) {
+                        Toast.makeText(MainActivity.this, "Stay There", Toast.LENGTH_SHORT).show();
+                    }
+                });
+                ad.setCancelable(false);
+                ad.show();
+
+            }
+        });
+    }
+}
