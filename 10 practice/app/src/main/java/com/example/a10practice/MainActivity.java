b2f78e059bc471a65f8f72058b1e530dab460c64
aamirali78654 <aamiralisargani786@gmail.com>
2024-08-19 22:46:14 +0500

    declair btn id

--- 10 practice/app/src/main/java/com/example/a10practice/MainActivity.java
+++ 10 practice/app/src/main/java/com/example/a10practice/MainActivity.java
@@ -1,6 +1,7 @@
 package com.example.a10practice;
 
 import android.os.Bundle;
+import android.widget.Button;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -11,12 +12,15 @@ import androidx.core.view.WindowInsetsCompat;
 import com.example.myapplication.R;
 
 public class MainActivity extends AppCompatActivity {
+    Button btn;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
         setContentView(R.layout.activity_main);
+        btn = findViewById(R.id.button);
+
 
 
 
