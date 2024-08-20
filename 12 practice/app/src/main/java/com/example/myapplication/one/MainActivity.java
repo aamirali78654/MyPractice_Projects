c73683646e34f5996dde85e41bd89ec0eda09eee
aamirali78654 <aamiralisargani786@gmail.com>
2024-08-21 00:06:48 +0500

    All fragments the usage for demo time

--- 12 practice/app/src/main/java/com/example/myapplication/one/MainActivity.java
+++ 12 practice/app/src/main/java/com/example/myapplication/one/MainActivity.java
@@ -7,20 +7,20 @@ import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
+import androidx.viewpager2.widget.ViewPager2;
 
 import com.example.myapplication.R;
 
 public class MainActivity extends AppCompatActivity {
-
+      ViewPager2 view_pager2;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
         setContentView(R.layout.activity_main);
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
-            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
-            return insets;
-        });
+        view_pager2 = findViewById(R.id.view_pager2);
+
+     MyAdapter myAdapter = new MyAdapter(getSupportFragmentManager(), getLifecycle());
+     view_pager2.setAdapter(myAdapter);
     }
 }
--- 12 practice/app/src/main/java/com/example/myapplication/one/MyAdapter.java
+++ 12 practice/app/src/main/java/com/example/myapplication/one/MyAdapter.java
@@ -1,2 +1,46 @@
-package com.example.myapplication.one;public class MyAdapter {
-}
+package com.example.myapplication.one;
+
+import androidx.annotation.NonNull;
+import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentActivity;
+import androidx.fragment.app.FragmentManager;
+import androidx.lifecycle.Lifecycle;
+import androidx.viewpager2.adapter.FragmentStateAdapter;
+
+public class MyAdapter extends FragmentStateAdapter
+
+     {
+
+         public MyAdapter(@NonNull FragmentActivity fragmentActivity) {
+             super(fragmentActivity);
+         }
+
+         public MyAdapter(FragmentManager supportFragmentManager, Lifecycle lifecycle) {
+             super(supportFragmentManager  ,lifecycle);
+         }
+
+         @NonNull
+         @Override
+         public Fragment createFragment(int position)
+         {
+             switch (position)
+             {
+                 case 0:
+                     FragmentOne one = new FragmentOne();
+                     return one;
+                 case 1:
+                     FragmentTwo two = new FragmentTwo();
+                     return two;
+                 case 2:
+                     FragmentThree three = new FragmentThree();
+                     return three;
+                 default:
+                     return null;
+             }
+         }
+
+         @Override
+         public int getItemCount() {
+             return 3;
+         }
+     }
