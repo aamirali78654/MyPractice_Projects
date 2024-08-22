05a878c254749fc3ad3e5a6054d8879c38e1280f
aamirali78654 <aamiralisargani786@gmail.com>
2024-08-23 00:20:58 +0500

    the layout for this fragment for settings

--- 14 practice/app/src/main/java/com/example/bottomnavigationv/One/FragmentFour.java
+++ 14 practice/app/src/main/java/com/example/bottomnavigationv/One/FragmentFour.java
@@ -15,7 +15,7 @@ public class FragmentFour extends Fragment {
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
-        // Inflate the layout for this fragment
+
         return inflater.inflate(R.layout.fragment_four, container, false);
     }
 }
