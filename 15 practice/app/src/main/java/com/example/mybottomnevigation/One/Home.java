c5e1cdedbc0c195d29fa87cceef6d148edf68048
aamirali78654 <aamiralisargani786@gmail.com>
2024-08-23 17:51:22 +0500

    Fragment and their objects many frameLayouts

--- 15 practice/app/src/main/java/com/example/mybottomnevigation/One/Home.java
+++ 15 practice/app/src/main/java/com/example/mybottomnevigation/One/Home.java
@@ -13,9 +13,10 @@ import com.example.mybottomnevigation.R;
 public class Home extends Fragment {
 
     @Override
-    public View onCreateView(LayoutInflater inflater, ViewGroup container,
-                             Bundle savedInstanceState) {
-        // Inflate the layout for this fragment
+    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState)
+
+    {
+
         return inflater.inflate(R.layout.fragment_home, container, false);
     }
 }
