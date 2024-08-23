a1aa307d40e36bff0fae4ded44f4dfc743946b7e
aamirali78654 <aamiralisargani786@gmail.com>
2024-08-23 17:48:28 +0500

    menu bar for icons and their names

---
+++ 15 practice/app/src/main/java/com/example/mybottomnevigation/One/MainActivity.java
@@ -0,0 +1,57 @@
+package com.example.mybottomnevigation.One;
+
+import android.os.Bundle;
+import android.view.MenuItem;
+import android.view.View;
+import android.widget.Toast;
+
+import androidx.activity.EdgeToEdge;
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.graphics.Insets;
+import androidx.core.view.ViewCompat;
+import androidx.core.view.WindowInsetsCompat;
+
+import com.example.mybottomnevigation.R;
+import com.google.android.material.bottomnavigation.BottomNavigationView;
+
+public class MainActivity extends AppCompatActivity {
+    BottomNavigationView btm_vavy;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        EdgeToEdge.enable(this);
+        setContentView(R.layout.activity_main);
+        btm_vavy = findViewById(R.id.btm_vavy);
+        getSupportFragmentManager().beginTransaction().replace(R.id.frame_layout , new Home()).commit();
+
+        btm_vavy.setOnItemSelectedListener(new BottomNavigationView.OnNavigationItemSelectedListener() {
+            @Override
+            public boolean onNavigationItemSelected(@NonNull MenuItem item)
+            {
+                if(item.getItemId() == R.id.home)
+                {
+                    getSupportFragmentManager().beginTransaction().replace(R.id.frame_layout , new Home()).commit();
+                }
+                else if(item.getItemId() == R.id.profile)
+                {
+                    getSupportFragmentManager().beginTransaction().replace(R.id.frame_layout , new Profile()).commit();
+                }
+                else if(item.getItemId() == R.id.message)
+                {
+                    getSupportFragmentManager().beginTransaction().replace(R.id.frame_layout , new Settings()).commit();
+                }
+                else if(item.getItemId() == R.id.setting)
+                {
+                    getSupportFragmentManager().beginTransaction().replace(R.id.frame_layout , new Message()).commit();
+                }
+                return true;
+            }
+        });
+    }
+    public void floatingbtn(View view)
+    {
+        Toast.makeText(this, "you clicked FLoating Button!!", Toast.LENGTH_SHORT).show();
+    }
+}
