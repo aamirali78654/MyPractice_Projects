5d29e06b97f5882a21d09e177cacb0bfd24a4c59
aamirali78654 <aamiralisargani786@gmail.com>
2024-08-24 14:01:55 +0500

    the layout for this fragment Settings

--- 16 practice/app/src/main/java/com/example/navigationdrawerpractice/One/settings.java
+++ 16 practice/app/src/main/java/com/example/navigationdrawerpractice/One/settings.java
@@ -15,7 +15,6 @@ public class settings extends Fragment {
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
-        // Inflate the layout for this fragment
         return inflater.inflate(R.layout.fragment_settings, container, false);
     }
 }
