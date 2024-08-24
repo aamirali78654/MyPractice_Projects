81ad7f6af703ed8a39ce1910ecaab09cf77a8712
aamirali78654 <aamiralisargani786@gmail.com>
2024-08-24 13:59:28 +0500

    the layout for this fragment Home

--- 16 practice/app/src/main/java/com/example/navigationdrawerpractice/One/home.java
+++ 16 practice/app/src/main/java/com/example/navigationdrawerpractice/One/home.java
@@ -15,7 +15,7 @@ public class home extends Fragment {
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
-        // Inflate the layout for this fragment
+
         return inflater.inflate(R.layout.fragment_home, container, false);
     }
 }
