65a514d4ee3d87f17c24a3d812e475c77f280d43
aamirali78654 <aamiralisargani786@gmail.com>
2024-08-26 21:14:47 +0500

    profile fragments to connect navigaton bar

--- 16 practice/app/src/main/java/com/example/navigationdrawerpractice/One/profile.java
+++ 16 practice/app/src/main/java/com/example/navigationdrawerpractice/One/profile.java
@@ -16,7 +16,7 @@ public class profile extends Fragment {
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState)
     {
-
+          //============ Profile ==================>>
         return inflater.inflate(R.layout.fragment_profile, container, false);
     }
 }
