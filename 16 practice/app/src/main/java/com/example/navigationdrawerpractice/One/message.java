c12d8c42ae9ebc93c8559aa3c14a31402cf3b0bc
aamirali78654 <aamiralisargani786@gmail.com>
2024-08-27 22:11:25 +0500

    fragments in line code

--- 16 practice/app/src/main/java/com/example/navigationdrawerpractice/One/message.java
+++ 16 practice/app/src/main/java/com/example/navigationdrawerpractice/One/message.java
@@ -15,7 +15,7 @@ public class message extends Fragment {
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState)
     {
-        //=================message====================>>
+        //=================message===============>>
         return inflater.inflate(R.layout.fragment_message, container, false);
     }
 }
