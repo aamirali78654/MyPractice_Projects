5ed9330ba5bfef8816ee00e9d36c613b5952dfa7
aamirali78654 <aamiralisargani786@gmail.com>
2024-08-28 16:43:48 +0500

    mainActivity java file

--- 20 practice/app/src/main/java/com/example/myapplication/MainActivity.java
+++ 20 practice/app/src/main/java/com/example/myapplication/MainActivity.java
@@ -15,6 +15,6 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
         setContentView(R.layout.activity_main);
-
+//-------------------mainActivity file java---------------------->>
     }
 }
