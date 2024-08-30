8ee73d703ed9acff8b62908e543d2df926564b8b
aamirali78654 <aamiralisargani786@gmail.com>
2024-08-30 23:18:14 +0500

    mainActivity include image button

--- 21 practice/app/src/main/java/com/example/myapplication/MainActivity.java
+++ 21 practice/app/src/main/java/com/example/myapplication/MainActivity.java
@@ -1,6 +1,7 @@
 package com.example.myapplication;
 
 import android.os.Bundle;
+import android.widget.ImageView;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -9,12 +10,13 @@ import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
 public class MainActivity extends AppCompatActivity {
-
+ImageView imageView ;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
         setContentView(R.layout.activity_main);
+        imageView = findViewById(R.id.imageView5);
 
     }
 }
