b7cad8db7fea1244aec23b363f6c54296210f821
aamirali78654 <aamiralisargani786@gmail.com>
2024-09-05 21:58:51 +0500

    MainActivity id's declair

--- 23 projects/app/src/main/java/com/example/practicebased/MainActivity.java
+++ 23 projects/app/src/main/java/com/example/practicebased/MainActivity.java
@@ -1,16 +1,21 @@
 package com.example.practicebased;
 
 import android.os.Bundle;
+import android.widget.EditText;
+
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
 
 public class MainActivity extends AppCompatActivity {
+    EditText editText , editText1;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
         setContentView(R.layout.activity_main);
+        editText = findViewById(R.id.textView);
+        editText1 = findViewById(R.id.textView2);
 
     }
 }
