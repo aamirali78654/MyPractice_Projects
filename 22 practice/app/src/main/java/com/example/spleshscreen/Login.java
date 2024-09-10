4cc1189288ecdbf627324ae95dab3bed945405ba
aamirali78654 <aamiralisargani786@gmail.com>
2024-09-10 22:51:52 +0500

    Login id put the code

--- 22 practice/app/src/main/java/com/example/spleshscreen/Login.java
+++ 22 practice/app/src/main/java/com/example/spleshscreen/Login.java
@@ -3,6 +3,8 @@ package com.example.spleshscreen;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.EditText;
+import android.widget.Toast;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -11,6 +13,7 @@ import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
 public class Login extends AppCompatActivity {
+    EditText log_email,log_password;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -18,11 +21,29 @@ public class Login extends AppCompatActivity {
         EdgeToEdge.enable(this);
         setContentView(R.layout.activity_login);
 
+
     }
     public void loginNow(View view)
     {
+        log_email = (EditText) findViewById(R.id.log_email);
+        log_password = (EditText) findViewById(R.id.log_password);
+        String log_e = log_email.getText().toString();
+        String log_p = log_password.getText().toString();
+
+        Register r = new Register();
+        String email =  r.reg_email.getText().toString();
+        String password = r.reg_password.getText().toString();
+        if(log_e == email && log_p == password)
+        {
+            Intent intent = new Intent(getApplicationContext(), Profile.class);
+            startActivity(intent);
+        }
+        else {
+            Toast.makeText(getApplicationContext(), "Please Registertion!!", Toast.LENGTH_SHORT).show();
+        }
 
     }
+
     public void notRegester(View view)
     {
         Intent intent = new Intent(getApplicationContext(), Register.class);
